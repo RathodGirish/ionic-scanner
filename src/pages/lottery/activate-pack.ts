d5a0053ca6993d8f03f64ad2f3da2a8be12dc14b
Girish R <rathodgirishk@gmail.com>
2017-05-20 10:06:04 +0530

    Fixed bin_no issue

--- src/pages/lottery/activate-pack.ts
+++ src/pages/lottery/activate-pack.ts
@@ -16,7 +16,7 @@ import { APIService } from '../../providers/api-service';
 
 export class ActivatePackPage {
   info: any;
-  public ActivatePackObject = { "ticketCode": "", "activateDate": "", "scan_ticket_code": "", "status": "activate", "bin_no": "" };
+  public ActivatePackObject = { "ticketCode": "", "activateDate": "", "scan_ticket_code": "", "status": "active", "bin_no": "" };
 
   public packs = [];
   public packCount;
@@ -40,7 +40,7 @@ export class ActivatePackPage {
     }
     let currentDate = this.commonService.getFormattedDateYMD(Date.now());
     this.ActivatePackObject.activateDate = currentDate;
-    THIS.API_SERVICE.getLatestPackByDate(this.info.store_id,this.info.company_id,currentDate, GLOBAL_VARIABLE.ACTIVATE_PACK_STATUS, function (err, res) {
+    THIS.API_SERVICE.getLatestPackByDate(this.info.store_id, this.info.company_id, currentDate, GLOBAL_VARIABLE.ACTIVATE_PACK_STATUS, function (err, res) {
       if (err) {
         console.log("ERROR!: ", err.message);
         THIS.isPacksFound = false;
@@ -59,11 +59,11 @@ export class ActivatePackPage {
     console.log('ionViewDidLoad Activate');
   }
 
-  public ActivatePack(){
-     let THIS = this;
-     THIS.commonService.showLoading();
-     console.log('ActivatePackObject ' + JSON.stringify(this.ActivatePackObject));
-     THIS.commonService.getGameNoAndPackNo(THIS.ActivatePackObject.ticketCode, function(err, game_no, pack_no){
+  public ActivatePack() {
+    let THIS = this;
+    THIS.commonService.showLoading();
+    console.log('ActivatePackObject ' + JSON.stringify(this.ActivatePackObject));
+    THIS.commonService.getGameNoAndPackNo(THIS.ActivatePackObject.ticketCode, function (err, game_no, pack_no) {
       let body = new FormData();
       body.append('game_no', game_no);
       body.append('store', THIS.info.store_id);
@@ -81,28 +81,27 @@ export class ActivatePackPage {
       let options = new RequestOptions({ headers: headers });
 
       THIS.API_SERVICE.activePack(body, options, function (err, res) {
-        alert('err ' + JSON.stringify(err));
-          if (err && err.message == 'notfound') {
-            THIS.commonService.showAlert('No Record Found');
+        if (err && err.message == 'notfound') {
+          THIS.commonService.showAlert('No Record Found');
+          THIS.navCtrl.push(AddGamePage);
+        } else if (err) {
+          THIS.commonService.showErrorAlert('ERROR!: ' + err.message);
+          THIS.navCtrl.push(AddGamePage);
+        } else {
+          if (THIS.commonService.isSuccess(res.status)) {
+            THIS.commonService.showSucessAlert('Pack Activated Successfully');
+            THIS.navCtrl.setRoot('ActivatePackPage');
+          } else {
+            THIS.commonService.showErrorAlert('Fail to Active Pack');
             THIS.navCtrl.push(AddGamePage);
-          } else if (err) {
-            THIS.commonService.showErrorAlert('ERROR!: ' + err.message);
-            THIS.navCtrl.push(AddGamePage);
-          }  else {
-            if (THIS.commonService.isSuccess(res.status)) {
-              THIS.commonService.showSucessAlert('Pack Activated Successfully');
-              THIS.navCtrl.setRoot('activePack');
-            } else {
-              THIS.commonService.showErrorAlert('Fail to Active Pack');
-              THIS.navCtrl.push(AddGamePage);
-            }
           }
-        });
-    }); 
+        }
+      });
+    });
 
   }
 
-async scanBarcode() {
+  async scanBarcode() {
     const results = await this.barcode.scan();
     if (results.text) {
       this.ActivatePackObject.ticketCode = results.text;
